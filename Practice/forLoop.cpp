7c41c19ba439fe67e62fc4aaae141ff44d6bf286
Noah <nbw2001@yahoo.com>
2021-10-27 10:31:31 -0700

    init

---
+++ Practice/forLoop.cpp
@@ -0,0 +1,20 @@
+#include<iostream> 
+
+using namespace std; 
+
+int main()
+
+{
+    int n; 
+    int i;
+    int sum;
+
+    cout << "Enter 10 numbers: ";
+    cin >> n;
+
+    for(i = 1; i <= 10; i++)
+    {
+        n += sum;
+        cin >> n;
+    }
+}
