5336e6dabff4c30ddabf0d26c04045d5c72bc0ae
Noah <nbw2001@yahoo.com>
2021-11-04 13:52:17 -0700

    update

--- Assignment/Assignment6_3.cpp
+++ Assignment/Assignment6_3.cpp
@@ -7,8 +7,6 @@ int main()
 {
     
     int grades[10];
-    //created a counter for the grade number
-    int counter = 1;
     //set highest to 0 and lowest to 101 so I do not have to initialize it later with grades[i];
     int highest = 0;
     int lowest = 101;
@@ -17,9 +15,8 @@ int main()
     for(int i = 0; i <= 9; i++)
     {
         
-        cout << "Enter grade " << counter << ": ";
+        cout << "Enter grade " << i + 1 << ": ";
         cin >> grades[i];
-        counter++;
         //all grades must be added to form a mean
         mean += grades[i];
 
@@ -33,9 +30,10 @@ int main()
             lowest = grades[i];
         }
     }
+    //remember to divide by 10 to get correct mean
+    mean /= 10;
 
     cout << "The highest grade is: " << highest << endl;
     cout << "The lowest grade is: " << lowest << endl;
-    //remember to divide by 10 to get correct mean
-    cout << "The mean grade is: " << mean / 10 << endl;
+    cout << "The mean grade is: " << mean << endl;
 }
